c0192c83fe0d7c5ccc51c4f258ae9f53c09cd3a7
lukkanog <lukanog@hotmail.com>
2022-01-03 22:48:29 -0300

    feat: create display component

---
+++ src/components/Display.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="display">
+        {{value}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Display',
+    props: {
+        value: {
+            type: String,
+            required: true
+        }
+    },
+}
+</script>
+
+<style> 
+.display{
+    grid-column: span 4;
+    background-color: #0005;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 20px;
+    font-size: 2.1em;
+    overflow: hidden;
+    color: #ddd;
+}
+</style>
